5d44871daa66abb4ba2807c0a12c2a06d7da1724
Duy_Khai_Pr0xY <duykhai.lu288@gmail.com>
2025-12-26 21:27:06 +0700

    update

---
+++ Ky_tu.php
@@ -0,0 +1,32 @@
+<pre>
+<?php
+
+# Đối với chuỗi thông thường thì okay, gần như hai ông này ko khác nhau lắm
+$nhaydon = 'aaa';
+$nhaykep = "bbb";
+
+echo "Nháy đơn: "; var_dump($nhaydon); echo "\n";
+echo "Nháy kép: "; var_dump($nhaykep); echo "\n";
+
+# Nhưng trong PHP, nháy đơn không được CHÈN BIẾN VÀO
+# nháy kép thì được
+
+$name = "CyberJutsu";
+$nhaydon = 'Hello $name - hihi';
+$nhaykep = "Hello $name - hihi"; # màu của $name ở dòng này, rõ ràng là khác
+
+echo "Nháy đơn: "; var_dump($nhaydon); echo "\n";
+echo "Nháy kép: "; var_dump($nhaykep); echo "\n";
+
+# Nháy đơn không được chèn các dạng encoding
+# Nháy kép thì được
+
+$name = "CyberJutsu";
+$nhaydon = 'Đây là chữ A: \x41';
+$nhaykep = "Đây là chữ A: \x41"; # màu của \x41 ở dòng này, rõ ràng là khác
+
+echo "Nháy đơn: "; var_dump($nhaydon); echo "\n";
+echo "Nháy kép: "; var_dump($nhaykep); echo "\n";
+
+?>
+</pre>
